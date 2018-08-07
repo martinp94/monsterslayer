f20945e7b18e3d7c78471219c5c9ec0453cfce2a
Martin Popovic <martinpopovicfit@gmail.com>
2018-08-07 19:20:39 +0200

    Attack functionality added

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ new Vue({
 			this.gaveUp = false;
 			this.healthMonster = 100;
 			this.healthYou = 100;
+		},
+		attack: function() {
+
+			const yourDamage = generateDamage(), monsterDamage = generateDamage();
+			
+			this.healthYou -= yourDamage; this.healthMonster -= monsterDamage;
+
+			function generateDamage() {
+				return Math.floor(Math.random() * 20) + 1;
+			}
+		},
+		specialAttack: function() {
+
+		},
+		heal: function() {
+
 		}
 	}
 
