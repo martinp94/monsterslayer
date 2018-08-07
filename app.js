07dcd644e5fa822eb1c9b38a741f0e772c871055
Martin Popovic <martinpopovicfit@gmail.com>
2018-08-07 20:19:37 +0200

    Heals added

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ new Vue({
 			margin: 0, 
 			color: 'white'
 		},
-		gaveUp: false
+		gaveUp: false,
+		log: [],
+		turns: {
+			'.log ul .player-turn': false,
+			'.log ul .monster-turn': false
+		}
 	},
 	methods: {
 		restart: function() {
@@ -16,6 +21,7 @@ new Vue({
 			this.gaveUp = false;
 			this.healthMonster = 100;
 			this.healthYou = 100;
+			this.log = [];
 		},
 		attack: function() {
 
@@ -23,17 +29,35 @@ new Vue({
 			
 			this.healthYou -= yourDamage; this.healthMonster -= monsterDamage;
 
+			this.log.push({
+				type: 'attack',
+				playerDmg: yourDamage,
+				monsterDmg: monsterDamage
+			});
+
 		},
 		specialAttack: function() {
 
 			const yourDamage = this.generateRandomInt(3), monsterDamage = this.generateRandomInt(30);
 
 			this.healthYou -= yourDamage; this.healthMonster -= monsterDamage;
+
+			this.log.push({
+				type: 'attack',
+				playerDmg: yourDamage,
+				monsterDmg: monsterDamage
+			});
 		},
 		heal: function() {
 			const heal = this.generateRandomInt(20), yourDamage = this.generateRandomInt(20);
 
 			this.healthYou += heal - yourDamage;
+
+			this.log.push({
+				type: 'heal',
+				playerDmg: yourDamage,
+				playerHeal: heal
+			});
 		},
 		generateRandomInt: function(max) {
 			return Math.floor(Math.random() * max) + 1;
