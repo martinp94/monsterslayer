6329b25eabaec3d00328075e58cec93071e47e3b
Martin Popovic <martinpopovicfit@gmail.com>
2018-08-07 18:39:55 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+new Vue({
+	el: "#app",
+	data: {
+		healthYou: 100,
+		healthMonster: 100,
+		healthbarStyles: {
+			background: 'green',
+			margin: 0, 
+			color: 'white'
+		}
+	},
+	methods: {
+
+	}
+
+});
